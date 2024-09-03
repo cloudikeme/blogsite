388dd1551fe2524762d05d3e962d6137bcba7841
cloudikeme <cloudikeme@gmail.com>
2024-09-03 10:50:35 +0100

    update

--- src/partials/Navbar.tsx
+++ src/partials/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar = () => (
 
       <NavMenu>
         <NavMenuItem href="/posts/">Blogs</NavMenuItem>
-        <NavMenuItem href="/posts/">Tutorials</NavMenuItem>
+        <NavMenuItem href="/posts/">Resume</NavMenuItem>
         <NavMenuItem href="/">GitHub</NavMenuItem>
         <NavMenuItem href="/">Twitter</NavMenuItem>
       </NavMenu>
