8bbb729e312328e3556b44f898fd6b17876ba728
cloudikeme <cloudikeme@gmail.com>
2024-09-03 10:26:43 +0100

    update

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
+  site_name: "Cloudikeme's Kube",
+  title: "Cloudikeme's Kube",
+  description: 'I Preach, Teach, and Do Cloud Native Things. ',
+  author: 'Victor Ikeme',
   locale_region: 'en-us',
   locale: 'en',
 };
